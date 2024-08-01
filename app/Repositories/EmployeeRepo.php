65ad734517d84e046a0be1a801c6016de1aef707
Ali Mohammadi <alimohammadi123450@gmail.com>
2024-08-01 21:08:26 +0330

    chnaged method Update in repo

--- app/Repositories/EmployeeRepo.php
+++ app/Repositories/EmployeeRepo.php
@@ -56,21 +56,11 @@ class EmployeeRepo implements EmployeeRepoInterface
 
     public function update($id, Request $request)
     {
-        $user = new User();
-        $user->name = trim($request->name);
-        $user->last_name = trim($request->last_name);
-        $user->email = trim($request->email);
-        $user->phone_number = trim($request->phone_number);
-        $user->hire_date = trim($request->hire_date);
-        $user->job_id = trim($request->job_id);
-        $user->salary = trim($request->salary);
-        $user->commision_pct = trim($request->commision_pct);
-        $user->manager_id = trim($request->manager_id);
-        $user->department_id = trim($request->department_id);
-        $user->is_role = 0;
-        //employees
-        // dd($user);
-        $user->save();
+        User::find($id)->update([
+            'name' => trim($request['name']),
+            'last_name' => trim($request['last_name']),
+        ]);
+       
     }
 
     private function employeeStore(User $employee,array $data)
